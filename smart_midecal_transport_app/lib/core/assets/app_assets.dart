6a9b7af04ba7e93f2d1978fa7fb2d8555b8b8be0
Khalid Abdelrazk <khalidabdelrazk990@gmail.com>
2025-12-07 13:33:41 +0200

    fully added onboarding screens

---
+++ smart_midecal_transport_app/lib/core/assets/app_assets.dart
@@ -0,0 +1,16 @@
+abstract class AppAssets {
+  static const String _imgPath = 'assets/images/';
+  static const String _svgPath = 'assets/svg/';
+
+  // todo : images =>
+  static const String onBoarding1 = '${_imgPath}onboarding1.png';
+  static const String onBoarding2 = '${_imgPath}onboarding2.png';
+  static const String onBoarding3 = '${_imgPath}onboarding3.png';
+  static const String onBoarding4 = '${_imgPath}onboarding4.png';
+
+  // --------------------------------------------------------------------------
+
+  // todo : svg Pictures =>
+  static const String egypt = '${_svgPath}ar.svg';
+
+}
--- smart_midecal_transport_app/lib/core/routes/route_names.dart
+++ smart_midecal_transport_app/lib/core/routes/route_names.dart
@@ -1,3 +1,4 @@
 class RouteNames{
   static const String register = 'register';
+  static const String onBoarding = 'onBoarding';
 }
