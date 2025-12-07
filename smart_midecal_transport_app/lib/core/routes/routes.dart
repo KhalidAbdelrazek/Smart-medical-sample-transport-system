a1979b98211b03c075cc312b2bbcabd07aa49e85
Khalid Abdelrazk <khalidabdelrazk990@gmail.com>
2025-12-07 05:54:59 +0200

    - continue on main initialization added error classes added theme provider for light and dark mode added routenames for navigating

---
+++ smart_midecal_transport_app/lib/core/routes/routes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class Routes {
+  static Map<String, Widget Function(BuildContext)> routes = {
+    // RouteNames.splash: (_) => SplashScreen(),
+    // RouteNames.homeScreen: (_) => RootApp(),
+  };
+}
