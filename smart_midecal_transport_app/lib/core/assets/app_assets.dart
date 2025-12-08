4720b86e4df8a2c608b7214a788f8cec026ac786
Khalid Abdelrazk <khalidabdelrazk990@gmail.com>
2025-12-08 13:14:38 +0200

    added onboarding logic and tested via simulator - added logos icons used in the sign in screen

--- smart_midecal_transport_app/lib/core/assets/app_assets.dart
+++ smart_midecal_transport_app/lib/core/assets/app_assets.dart
@@ -7,10 +7,16 @@ abstract class AppAssets {
   static const String onBoarding2 = '${_imgPath}onboarding2.png';
   static const String onBoarding3 = '${_imgPath}onboarding3.png';
   static const String onBoarding4 = '${_imgPath}onboarding4.png';
+  
+  static const String appLogo = '${_imgPath}logo.png';
+
+  static const String signInEmployeeIcon =
+      '${_imgPath}sign-in-employee-icon.png';
+  static const String signInEmployerIcon =
+      '${_imgPath}sign-in-employer-icon.png';
 
   // --------------------------------------------------------------------------
 
   // todo : svg Pictures =>
   static const String egypt = '${_svgPath}ar.svg';
-
 }
