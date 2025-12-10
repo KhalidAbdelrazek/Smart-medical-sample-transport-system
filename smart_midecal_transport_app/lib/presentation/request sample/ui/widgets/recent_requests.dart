864a92114b674a22b330672e05a28348a2dfaf05
Khalid Abdelrazk <khalidabdelrazk990@gmail.com>
2025-12-10 17:53:37 +0200

    request blood page finished

--- smart_midecal_transport_app/lib/presentation/request sample/ui/widgets/recent_requests.dart
+++ smart_midecal_transport_app/lib/presentation/request sample/ui/widgets/recent_requests.dart
@@ -10,6 +10,7 @@ class RecentRequestsList extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return Column(
+      textDirection: TextDirection.ltr,
       children: requestBloodViewModel.recentRequests.map((e) {
         return Container(
           margin: EdgeInsets.only(bottom: 15.h),
@@ -17,22 +18,40 @@ class RecentRequestsList extends StatelessWidget {
           decoration: BoxDecoration(
             color: theme.cardColor,
             borderRadius: BorderRadius.circular(18.r),
+            border: BoxBorder.all(color: theme.highlightColor, width: 0.5),
           ),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            textDirection: TextDirection.ltr,
+
             children: [
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
+                textDirection: TextDirection.ltr,
                 children: [
-                  Text(e["name"]!, style: theme.textTheme.headlineSmall),
+                  Text(
+                    e["name"]!,
+                    style: theme.textTheme.headlineSmall,
+                    textDirection: TextDirection.ltr,
+                  ),
                   SizedBox(height: 6.h),
-                  Text(e["blood"]!, style: theme.textTheme.bodyMedium),
+                  Text(
+                    e["blood"]!,
+                    style: theme.textTheme.bodyMedium,
+                    textDirection: TextDirection.ltr,
+                  ),
                   SizedBox(height: 6.h),
                   Row(
+                    textDirection: TextDirection.ltr,
+
                     children: [
                       const Icon(Icons.access_time, size: 16),
                       SizedBox(width: 6.w),
-                      Text(e["time"]!, style: theme.textTheme.bodySmall),
+                      Text(
+                        e["time"]!,
+                        style: theme.textTheme.bodySmall,
+                        textDirection: TextDirection.ltr,
+                      ),
                     ],
                   ),
                 ],
@@ -40,7 +59,9 @@ class RecentRequestsList extends StatelessWidget {
               Text(
                 e["status"]!,
                 style: theme.textTheme.bodyMedium?.copyWith(
-                  color: e["status"] == "pending" ? Colors.orange : Colors.green,
+                  color: e["status"] == "Pending"
+                      ? Colors.orange
+                      : Colors.green,
                   fontWeight: FontWeight.bold,
                 ),
               ),
