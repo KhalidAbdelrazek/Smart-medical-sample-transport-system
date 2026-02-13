121f08341dc046a0f751bea31d11561487032970
Khalid Abdelrazk <khalidabdelrazk990@gmail.com>
2026-02-13 21:57:40 +0200

    some edits on behaves

--- smart_midecal_transport_app/lib/main.dart
+++ smart_midecal_transport_app/lib/main.dart
@@ -79,7 +79,8 @@ class MyApp extends StatelessWidget {
 
           // App navigation
           routes: Routes.routes,
-          initialRoute: SharedPrefService.instance.onBoardingViewed() ?? false ? RouteNames.employerMainScreen : RouteNames.onBoarding,
+          initialRoute: RouteNames.employerMainScreen,
+          // initialRoute: SharedPrefService.instance.onBoardingViewed() ?? false ? RouteNames.root : RouteNames.onBoarding,
         );
       },
     );
