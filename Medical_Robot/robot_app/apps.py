c0f955a778ab2f06e30a61a7aef4a1d3ce8dddc8
Mohamed Ashraf <mohamed.ashraf.aboutaleb@gmail.com>
2026-02-24 23:55:10 +0200

    Removing robot_app

--- Medical_Robot/robot_app/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class RobotAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'robot_app'
-
-    def ready(self):
-        import robot_app.signals
