af5fdf9ad865d036cbdba2f89c6fb4514256127b
Mohamed Ashraf <mohamed.ashraf.aboutaleb@gmail.com>
2026-02-25 10:19:35 +0200

    Deleting the duplicated classes and logic between the two apps

--- Medical_Robot/healthcare/models.py
+++ Medical_Robot/healthcare/models.py
@@ -4,44 +4,6 @@ from django.utils import timezone
 from datetime import timedelta
 # Create your models here.
 
-class Patient(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, help_text="Unique UUID for the patient.")
-    name = models.CharField(max_length=100, help_text="The patient's full name.")
-    age = models.IntegerField(help_text="The patient's age in years.")
-    gender = models.CharField(max_length=10, help_text="The patient's gender (e.g., Male, Female).")
-    description = models.TextField(max_length=500, null=True, blank=True, help_text="Additional medical details or notes about the patient.")
-    phone = models.CharField(max_length=15, help_text="Primary contact number for the patient.")
-    email = models.EmailField(help_text="Patient's email address.")
-    password = models.CharField(max_length=100, help_text="Hashed password for patient login.")
-    city = models.CharField(max_length=50, help_text="City of residence.")
-    address = models.CharField(max_length=100, help_text="Full street address.")
-    zip_code = models.CharField(max_length=10, help_text="Postal zip code.")
-    
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    
-    def __str__(self):
-        return self.name
-
-
-class Staff(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, help_text="Unique UUID for the staff member.")
-    name = models.CharField(max_length=100, help_text="The full name of the staff member.")
-    age = models.IntegerField(help_text="The staff member's age in years.")
-    gender = models.CharField(max_length=10, help_text="The staff member's gender.")
-    role = models.CharField(max_length=50, help_text="The medical or administrative role of the staff member.")
-    phone = models.CharField(max_length=15, help_text="Primary contact number.")
-    city = models.CharField(max_length=50, help_text="City of residence.")
-    address = models.CharField(max_length=100, help_text="Full street address.")
-    zip_code = models.CharField(max_length=10, help_text="Postal zip code.")
-    email = models.EmailField(help_text="Staff member's email address.")
-    password = models.CharField(max_length=100, help_text="Hashed password for staff login.")
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    
-    def __str__(self):
-        return self.name
-
   
 class SensorReading(models.Model):
     # STATE_CHOICES = [
