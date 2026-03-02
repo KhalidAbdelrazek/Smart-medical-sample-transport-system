fd5572dd84b9e3cd43c79ad5da56bf9995450613
Khalid Abdelrazk <khalidabdelrazk990@gmail.com>
2026-03-02 23:43:51 +0200

    Config: Update Routing, API Endpoints, SharedPrefs, and DI configuration

--- smart_midecal_transport_app/lib/core/routes/route_names.dart
+++ smart_midecal_transport_app/lib/core/routes/route_names.dart
@@ -1,8 +1,8 @@
-class RouteNames{
+class RouteNames {
   static const String register = 'register';
   static const String onBoarding = 'onBoarding';
   static const String root = 'root';
+  static const String adminLogin = 'adminLogin';
   static const String storageScreen = 'storageScreen';
   static const String employerMainScreen = 'employerMainScreen';
-
 }
