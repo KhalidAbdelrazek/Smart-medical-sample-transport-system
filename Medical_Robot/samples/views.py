25773a1ab974c7e10ed889518d63eb26c8967659
Mohamed Ashraf <mohamed.ashraf.aboutaleb@gmail.com>
2026-03-22 00:02:05 +0200

    modify error message

--- Medical_Robot/samples/views.py
+++ Medical_Robot/samples/views.py
@@ -258,7 +258,7 @@ class BulkRequestSampleView(APIView):
                     }
                 )
             except Exception as e:
-                failed_requests.append({"sample_code": sample_code, "error": str(e)})
+                failed_requests.append({"sample_code": sample_code, "error": str(e.args)})
 
         # Determine response status
         if not successful_requests:
