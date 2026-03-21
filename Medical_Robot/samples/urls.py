869552821eb4e20647807ea7cdc59a569cb6db61
Mohamed Ashraf <mohamed.ashraf.aboutaleb@gmail.com>
2026-03-21 21:42:31 +0200

    Adding a class in the samples application that handles creating new blood samples

--- Medical_Robot/samples/urls.py
+++ Medical_Robot/samples/urls.py
@@ -3,15 +3,20 @@ from .views import (
     BloodSampleDetailView,
     RequestSampleView,
     BloodSampleSearchView,
+    CreateBloodSampleView,
 )
 
 urlpatterns = [
     # GET /api/samples/search/?q=
     path('search/', BloodSampleSearchView.as_view(), name='sample-search'),
+
+    # POST /api/samples/create/
+    path('create/', CreateBloodSampleView.as_view(), name='sample-create'),
     
     # POST /api/samples/request/
     path('request/', RequestSampleView.as_view(), name='sample-request'),
     
     # GET /api/samples/{sample_code}/
     path('<str:sample_code>/', BloodSampleDetailView.as_view(), name='sample-detail'),
+
 ]
