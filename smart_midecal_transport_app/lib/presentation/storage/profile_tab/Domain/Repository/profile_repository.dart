0a290d1500a5cc2bf8667ac9ec586da2007064a2
Khalid Abdelrazk <khalidabdelrazk990@gmail.com>
2026-03-22 02:01:19 +0200

    restructured design pattern folders

--- smart_midecal_transport_app/lib/presentation/storage/profile_tab/Domain/Repository/profile_repository.dart
+++ smart_midecal_transport_app/lib/presentation/storage/profile_tab/Domain/Repository/profile_repository.dart
@@ -1,8 +1,7 @@
+import 'package:either_dart/either.dart';
+import 'package:smart_midecal_transport_app/core/error/failures.dart';
+import 'package:smart_midecal_transport_app/presentation/storage/profile_tab/Domain/Entity/get_profle_entity.dart';
+
 abstract class ProfileRepository {
-  // Future<Either<Failures, LoginEmployeeResponseEntity>> loginEmployee(
-  //   LoginEmployeeRequestBodyEntity loginEmployeeRequestBodyEntity,
-  // );
-  // Future<Either<Failures, LoginAdminResponseEntity>> loginAdmin(
-  //   LoginAdminRequestBodyEntity loginAdminRequestBodyEntity,
-  // );
+  Future<Either<Failures, GetProfileEntity>> getProfile();
 }
