86b88db7a5fdd15e4c629f67d85130e554a0c037
Khalid Abdelrazk <khalidabdelrazk990@gmail.com>
2026-03-22 02:01:19 +0200

    restructured design pattern folders

--- smart_midecal_transport_app/lib/presentation/storage/profile_tab/Data/Data Sources/remote/profile_ds.dart
+++ smart_midecal_transport_app/lib/presentation/storage/profile_tab/Data/Data Sources/remote/profile_ds.dart
@@ -1,8 +1,7 @@
+import 'package:either_dart/either.dart';
+import 'package:smart_midecal_transport_app/core/error/failures.dart';
+import 'package:smart_midecal_transport_app/presentation/storage/profile_tab/Data/Models/get_profle_dm.dart';
+
 abstract class ProfileDataSource {
-  // Future<Either<Failures, LoginEmployeeResponseDm>> loginEmployee(
-  //   LoginEmployeeRequestBodyDm loginEmployeeRequestBodyDm,
-  // );
-  // Future<Either<Failures, LoginAdminResponseDm>> loginAdmin(
-  //   LoginAdminRequestBodyDm loginAdminRequestBodyDm,
-  // );
+  Future<Either<Failures, GetProfileDm>> getProfile();
 }
--- smart_midecal_transport_app/lib/presentation/storage/profile_tab/Data/Repository/profile_repository_impl.dart
+++ smart_midecal_transport_app/lib/presentation/storage/profile_tab/Data/Repository/profile_repository_impl.dart
@@ -1,10 +1,20 @@
+import 'package:either_dart/either.dart';
 import 'package:injectable/injectable.dart';
+import 'package:smart_midecal_transport_app/core/error/failures.dart';
+import 'package:smart_midecal_transport_app/presentation/storage/profile_tab/Data/Data%20Sources/remote/profile_ds.dart';
+import 'package:smart_midecal_transport_app/presentation/storage/profile_tab/Domain/Entity/get_profle_entity.dart';
 import 'package:smart_midecal_transport_app/presentation/storage/profile_tab/Domain/Repository/profile_repository.dart';
 
 @Injectable(as: ProfileRepository)
-class AuthRepositoryImpl extends ProfileRepository {
-  // final AuthRemoteDataSource authRemoteDataSource;
-  // AuthRepositoryImpl({required this.authRemoteDataSource});
+class ProfileRepositoryImpl extends ProfileRepository {
+  ProfileDataSource profileDataSource;
+  ProfileRepositoryImpl({required this.profileDataSource});
+  
+  @override
+  Future<Either<Failures, GetProfileEntity>> getProfile() {
+    // TODO: implement getProfile
+    throw UnimplementedError();
+  }
 
   // @override
   // Future<Either<Failures, LoginEmployeeResponseEntity>> loginEmployee(
