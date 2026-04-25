24e7ed9c60833ada6e58df066b3c55902ce5d0a4
mhmd-tarek-123 <m.belbel.2017@gmail.com>
2026-04-25 21:18:47 +0300

    modify car number

--- smart_midecal_transport_app/lib/presentation/employee/home/ui/home_tab.dart
+++ smart_midecal_transport_app/lib/presentation/employee/home/ui/home_tab.dart
@@ -5,13 +5,11 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:smart_midecal_transport_app/core/di/di.dart';
 import 'package:smart_midecal_transport_app/core/theme/color.dart';
 import 'package:smart_midecal_transport_app/presentation/storage/home_tab/ui/widgets/advanced_donut_chart.dart';
-import 'package:smart_midecal_transport_app/presentation/employee/home/ui/widgets/employee_donut_chart.dart';
 
 import 'cubit/employee_home_cubit.dart';
 import 'cubit/employee_home_state.dart';
 import 'widgets/employee_stats_card.dart';
-import 'widgets/employee_request_chart.dart';
-import 'widgets/employee_summary_card.dart';
+
 
 /// Employee Home Dashboard Tab
 /// Skeleton only shows on first load, not on pull-to-refresh
