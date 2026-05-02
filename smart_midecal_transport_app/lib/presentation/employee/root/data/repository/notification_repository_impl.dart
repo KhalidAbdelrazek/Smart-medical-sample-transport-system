5fdfeb7716bda73564570e1ac30c8d8e2b10618e
Khalid Abdelrazk <khalidabdelrazk4@gmail.com>
2026-05-02 04:11:07 +0300

    feat: implement notification data source and repository

--- smart_midecal_transport_app/lib/presentation/employee/root/data/repository/notification_repository_impl.dart
+++ smart_midecal_transport_app/lib/presentation/employee/root/data/repository/notification_repository_impl.dart
@@ -1,14 +1,15 @@
 import 'package:either_dart/either.dart';
+import 'package:injectable/injectable.dart';
 import 'package:smart_midecal_transport_app/core/error/failures.dart';
 import 'package:smart_midecal_transport_app/presentation/employee/root/data/data%20source/notification_ds.dart';
 import 'package:smart_midecal_transport_app/presentation/employee/root/domain/entity/notification_response_entity.dart';
 import 'package:smart_midecal_transport_app/presentation/employee/root/domain/repository/notification_repository.dart';
 
+@Injectable(as: NotificationRepository)
 class NotificationRepositoryImpl implements NotificationRepository {
-
   final NotificationDataSource notificationDataSource;
-  
-  NotificationRepositoryImpl(this.notificationDataSource);
+
+  NotificationRepositoryImpl({required this.notificationDataSource});
   @override
   Future<Either<Failures, NotificationResponseEntity>> getNotifications() {
     return notificationDataSource.getNotifications();
