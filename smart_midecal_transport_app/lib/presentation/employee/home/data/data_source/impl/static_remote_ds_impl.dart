5644183c7882fad93b900e6ae25b8fac305c5da3
Khalid Abdelrazk <khalidabdelrazk4@gmail.com>
2026-05-16 02:16:25 +0300

    localize employee home tab

--- smart_midecal_transport_app/lib/presentation/employee/home/data/data_source/impl/static_remote_ds_impl.dart
+++ smart_midecal_transport_app/lib/presentation/employee/home/data/data_source/impl/static_remote_ds_impl.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:dio/dio.dart';
 import 'package:either_dart/either.dart';
@@ -51,7 +52,7 @@ class EmploeeStatisticsRemoteDataSourceImpl
           ),
         );
       } else {
-        return Left(NetworkError(errorMessage: "Network Error"));
+        return Left(NetworkError(errorMessage: 'errors.network_error'.tr()));
       }
     } catch (e) {
       return Left(ServerError(errorMessage: e.toString()));
