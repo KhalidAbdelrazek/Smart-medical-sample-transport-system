5e430ecb41665c985e2daf673640a309163e371b
Khalid Abdelrazk <khalidabdelrazk4@gmail.com>
2026-05-16 20:43:00 +0300

    refactor employee notification data sources

--- smart_midecal_transport_app/lib/presentation/employee/root/data/data source/notification_ds.dart
+++ smart_midecal_transport_app/lib/presentation/employee/root/data/data source/notification_ds.dart
@@ -4,7 +4,9 @@ import 'package:smart_midecal_transport_app/presentation/employee/root/domain/en
 
 abstract class NotificationDataSource {
   Future<Either<Failures, NotificationResponseEntity>> getNotifications();
-  Future<Either<Failures, String?>> confirmDelivery({required String requestId});
+  Future<Either<Failures, String?>> confirmDelivery({
+    required String requestId,
+  });
   Future<Either<Failures, String?>> rejectDelivery({required String requestId});
 
   /// POST /api/transport/confirm-return-handoff
