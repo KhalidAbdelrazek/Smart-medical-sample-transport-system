1ce0e71d2970d06db575cf0c974afd8104acee61
Khalid Abdelrazk <khalidabdelrazk4@gmail.com>
2026-05-16 20:40:18 +0300

    localize employee home repositories

--- smart_midecal_transport_app/lib/presentation/employee/home/data/repos/static_repo.dart
+++ smart_midecal_transport_app/lib/presentation/employee/home/data/repos/static_repo.dart
@@ -5,8 +5,6 @@ import 'package:smart_midecal_transport_app/presentation/employee/home/data/data
 import 'package:smart_midecal_transport_app/presentation/employee/home/domain/entites/static_entity.dart';
 import 'package:smart_midecal_transport_app/presentation/employee/home/domain/repos/static_repo.dart';
 
-
-
 @Injectable(as: EmploeeStatisticsRepo)
 class EmployeeStatisticsRepoImpl implements EmploeeStatisticsRepo {
   final EmploeeStatisticsRemoteDataSource remote;
