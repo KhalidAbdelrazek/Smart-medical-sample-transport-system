7993f9007599da3d89d42c2f4ed0bd8b35ac4fcb
Khalid Abdelrazk <khalidabdelrazk4@gmail.com>
2026-05-16 20:43:23 +0300

    refactor storage notifications data source

--- smart_midecal_transport_app/lib/presentation/storage/storage_main/data/data source/notification_ds_impl.dart
+++ smart_midecal_transport_app/lib/presentation/storage/storage_main/data/data source/notification_ds_impl.dart
@@ -9,16 +9,13 @@ import 'package:smart_midecal_transport_app/core/utils/shared_pref_services.dart
 import 'package:smart_midecal_transport_app/presentation/storage/storage_main/data/data%20source/notification_ds.dart';
 import 'package:smart_midecal_transport_app/presentation/storage/storage_main/data/model/confirm_car_return_dm.dart';
 import 'package:smart_midecal_transport_app/presentation/storage/storage_main/data/model/returned_cars_response_dm.dart';
-import 'package:smart_midecal_transport_app/presentation/storage/storage_main/domain/entity/confirm_car_return_entity.dart';
-import 'package:smart_midecal_transport_app/presentation/storage/storage_main/domain/entity/returned_cars_response_entity.dart';
 
-class NotificationDsImpl extends NotificationDs{
+class NotificationDsImpl extends NotificationDs {
   final ApiManager apiManager;
   NotificationDsImpl({required this.apiManager});
-  
+
   @override
-  Future<Either<Failures, ReturnedCarsResponseDm>> getReturnedCars() async{
-   
+  Future<Either<Failures, ReturnedCarsResponseDm>> getReturnedCars() async {
     final List<ConnectivityResult> connectivityResult = await Connectivity()
         .checkConnectivity();
 
@@ -35,28 +32,29 @@ class NotificationDsImpl extends NotificationDs{
             validateStatus: (status) => true,
           ),
         );
-        print("response.data: ${response.data}");
 
         // Map the entire response (success, message, data, errors)
-        ReturnedCarsResponseDm returnedCarsResponseDm = ReturnedCarsResponseDm.fromJson(response.data);
+        ReturnedCarsResponseDm returnedCarsResponseDm =
+            ReturnedCarsResponseDm.fromJson(response.data);
 
         // 1. Check for specific Token Expiration / Invalid Token
         if (returnedCarsResponseDm.errors?.code == "token_not_valid") {
-          return Left(
-            ServerError(errorMessage: 'extra.session_expired'.tr()),
-          );
+          return Left(ServerError(errorMessage: 'extra.session_expired'.tr()));
         }
 
         // 2. Check for General Success (2xx status codes)
         if (response.statusCode! >= 200 &&
             response.statusCode! < 300 &&
-            returnedCarsResponseDm.success==true) {
+            returnedCarsResponseDm.success == true) {
           return Right(returnedCarsResponseDm);
         }
 
         // 3. Handle other Server Errors
         return Left(
-          ServerError(errorMessage: returnedCarsResponseDm.message?.toString() ?? "Server Error"),
+          ServerError(
+            errorMessage:
+                returnedCarsResponseDm.message?.toString() ?? "Server Error",
+          ),
         );
       } else {
         return Left(NetworkError(errorMessage: 'extra.no_internet_lower'.tr()));
@@ -66,11 +64,12 @@ class NotificationDsImpl extends NotificationDs{
         ServerError(errorMessage: "Unexpected Error: ${e.toString()}"),
       );
     }
-    
   }
 
   @override
-  Future<Either<Failures, ConfirmCarReturnDm>> confirmReturnedCar(int carId) async {
+  Future<Either<Failures, ConfirmCarReturnDm>> confirmReturnedCar(
+    int carId,
+  ) async {
     final List<ConnectivityResult> connectivityResult = await Connectivity()
         .checkConnectivity();
 
@@ -88,27 +87,28 @@ class NotificationDsImpl extends NotificationDs{
             validateStatus: (status) => true,
           ),
         );
-        print("response.data: ${response.data}");
 
         // Map the entire response (success, message, data, errors)
-        ConfirmCarReturnDm confirmCarReturnDm = ConfirmCarReturnDm.fromJson(response.data);
+        ConfirmCarReturnDm confirmCarReturnDm = ConfirmCarReturnDm.fromJson(
+          response.data,
+        );
 
         // 1. Check for specific Token Expiration / Invalid Token
         if (confirmCarReturnDm.errors?.code == "token_not_valid") {
-          return Left(
-            ServerError(errorMessage: 'extra.session_expired'.tr()),
-          );
+          return Left(ServerError(errorMessage: 'extra.session_expired'.tr()));
         }
 
         // 2. Check for General Success (2xx status codes)
-        if (response.statusCode! >= 200 &&
-            response.statusCode! < 300) {
+        if (response.statusCode! >= 200 && response.statusCode! < 300) {
           return Right(confirmCarReturnDm);
         }
 
         // 3. Handle other Server Errors
         return Left(
-          ServerError(errorMessage: confirmCarReturnDm.message?.toString() ?? "Server Error"),
+          ServerError(
+            errorMessage:
+                confirmCarReturnDm.message?.toString() ?? "Server Error",
+          ),
         );
       } else {
         return Left(NetworkError(errorMessage: 'extra.no_internet_lower'.tr()));
